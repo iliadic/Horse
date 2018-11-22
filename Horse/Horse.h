724df2509fb9ef9a481435582b811ef647edf25c
Vincent Esche <regexident@gmail.com>
2018-11-22 10:26:10 +0100

    Initial Commit

---
+++ Horse/Horse.h
@@ -0,0 +1,19 @@
+//
+//  Horse.h
+//  Horse
+//
+//  Created by Vincent Esche on 11/22/18.
+//  Copyright © 2018 Vincent Esche. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Horse.
+FOUNDATION_EXPORT double HorseVersionNumber;
+
+//! Project version string for Horse.
+FOUNDATION_EXPORT const unsigned char HorseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Horse/PublicHeader.h>
+
+
